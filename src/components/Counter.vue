7cefeed712d7e5170c60ae289b61c7f631ca7dd0
Kerlli <func.map0to1@gmail.com>
2021-07-09 21:38:25 +0800

    feat: update dev dep: vite from @2.4.0 to @2.4.1

---
+++ src/components/Counter.vue
@@ -0,0 +1,28 @@
+<template>
+  <p>counter: {{ count }}</p>
+  <el-button @click="increment">INCREMENT</el-button>
+  <el-button @click="decrement">DECREMENT</el-button>
+</template>
+
+<script>
+export default {
+  name: "Counter",
+  data() {
+    return {
+      count: 0
+    }
+  },
+  methods: {
+    increment() {
+      this.count++
+    },
+    decrement() {
+      this.count--
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
