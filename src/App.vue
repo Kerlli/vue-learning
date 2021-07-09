8bf038a71d5a499f178c8771699597b4eada9e4a
Kerlli <func.map0to1@gmail.com>
2021-07-09 21:41:51 +0800

    remove default template vue component <HelloWorld>

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <Counter/>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
+import Counter from './components/Counter.vue'
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
+    Counter,
   }
 })
 </script>
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
+import { ElButton } from 'element-plus'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(ElButton)
+
+app.mount('#app')
